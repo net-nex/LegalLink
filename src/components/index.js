7010877b5e318b573f6085fde3792f329b21fcb8
Ritam Biswas <ritambiswas1801@gmail.com>
2023-09-15 16:57:35 +0530

    updating pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,9 @@ import Footer from './Footer';
 import Hero from './Hero/Hero';
 import Lawyers from './Lawyers/Lawyers';
 import LawyersCard from './Lawyers/LawyersCard';
+import Categories from './Categories/Categories';
+import BottomCta from './BottomCta/BottomCta';
+import Cities from './Cities/Cities';
 
 export {
     Navbar,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-<<<<<<< HEAD
-import {Navbar,Footer, Categories, Cities, BottomCta} from '../components';
-=======
-import {Navbar,Footer,Hero,Lawyers} from '../components';
->>>>>>> b2e48895d02949acd4e5be5df725604ffc1a3e56
+import {Navbar,Footer, Categories, Cities, Hero, Lawyers, BottomCta} from '../components';
 
 const Home = () => {
   return (
     <div>
-<<<<<<< HEAD
-
-=======
-    
->>>>>>> b2e48895d02949acd4e5be5df725604ffc1a3e56
+      <Navbar/>
+      <Hero/>
+      <Lawyers/>
+      <Categories/>
+      <Cities/>
+      <BottomCta/>
+      <Footer/>
     </div>
   )
 }
