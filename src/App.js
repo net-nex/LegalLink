7aacb2ae26672a7e04b27777b1117b48dcc7639a
Gyanendar <gyantiwari10101@gmail.com>
2023-09-15 13:50:06 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { Routes, Route} from 'react-router-dom';
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path='/signin' element={<SignIn/>}/>
         <Route path='signup'  element={<SignUp/>}/>
       </Routes>
-      <Footer />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Navbar,Footer} from '../components';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
