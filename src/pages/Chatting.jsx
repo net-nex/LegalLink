733d07be6c712f34e3ed7efb210cf335b9590fda
Rushit Patel <rushitpatel163@gmail.com>
2023-09-16 14:38:47 +0530

    fiixing error

--- src/pages/Chatting.jsx
+++ src/pages/Chatting.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import {Sidebar} from '../components/Chat';
 import {Chat} from '../components/Chat';
 import {Navbar,Footer} from '../components/';
-
+import { Children, useContext } from "react";
+import { AuthContext } from "../context/authContext";
+import { useNavigate } from 'react-router-dom';
 const Chatting = () => {
+  const navigate = useNavigate
+  const { currentUser,lawyer } = useContext(AuthContext);
+  if(!currentUser){
+    return navigate('/signup')
+  }
   return (
     <>
         <Navbar />
